b6fe0d214d3da90aa52465550c39e9787cd18f6e
JimmyDmtrx <96940667+JimmyDmtrx@users.noreply.github.com>
2022-09-01 15:06:51 +0200

    sectors

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+import banquePic from '../styles/images/secteurs/banque.jpg';
+import artPic from '../styles/images/secteurs/art.jpg';
+import eventPic from '../styles/images/secteurs/event.jpg';
+import luxePic from '../styles/images/secteurs/luxe.jpg';
+import autoPic from '../styles/images/secteurs/auto.jpg';
+import ecomPic from '../styles/images/secteurs/ecom.jpg';
+import agriPic from '../styles/images/secteurs/agri.jpg';
+import hotelPic from '../styles/images/secteurs/hotel.jpg';
+import digiPic from '../styles/images/secteurs/tech.jpg';
+
+let sectors = [
+  {
+    name: 'Bancaire & Services de paiement',
+    imgurl: banquePic,
+  },
+  { name: "Marché de l'art", imgurl: artPic },
+  { name: 'Évènementiel', imgurl: eventPic },
+  { name: 'Luxe', imgurl: luxePic },
+  { name: 'Automobile & Transport', imgurl: autoPic },
+  { name: 'E-commerce', imgurl: ecomPic },
+  { name: 'Agriculture', imgurl: agriPic },
+  { name: 'Hôtellerie & restauration', imgurl: hotelPic },
+  { name: 'Tech & digital', imgurl: digiPic },
+];
+
+export default sectors;
--- src/pages/Secteurs.js
+++ src/pages/Secteurs.js
@@ -1,35 +1,39 @@
-import React from "react";
-import books from "../styles/images/books.jpg";
+import React from 'react';
+import sectors from '../data/data';
 
 const Secteurs = () => {
   return (
     <div className="secteurContent container">
-      <section className="row ">
-        <article className="col-4 mt-5">
-          <img style={{ width: "100%" }} src={books} alt="books" />
-        </article>
-        <article className="col-8 mt-5">
+      <section className="row">
+        <article className="mt-5">
           <p>
-            {" "}
+            {' '}
             Devilder Avocats déploie ses expertises au profit de clients
             intervenant dans divers domaines d’activité et s'adapte à tout
             secteur d'activité, ce qui lui a permis d’acquérir une expertise «
             métier » dans les domaines concernés.
           </p>
           <p>Il s’agit notamment des secteurs d’activité suivants :</p>
+        </article>
+        <article className="sector-listing">
           <ul className="row mt-5 d-flex justify-content-between">
-            <li className="col-5 mb-4 p-3 rounded-pill">Tech - Numérique</li>
-            <li className="col-5 mb-4 p-3 rounded-pill">Vente en ligne</li>
-            <li className="col-5 mb-4 p-3 rounded-pill ">
-              Bancaire - Services de paiement
-            </li>
-            <li className="col-5 mb-4 p-3 rounded-pill">Transport</li>
-            <li className="col-5 mb-4 p-3 rounded-pill">Automobile</li>
-            <li className="col-5 mb-4 p-3 rounded-pill">Agriculture</li>
-            <li className="col-5 mb-4 p-3 rounded-pill">Vente aux enchères</li>
-            <li className="col-5 mb-4 p-3 rounded-pill">
-              Hôtellerie-restauration
-            </li>
+            {sectors.map((sector, index) => {
+              return (
+                <li
+                  key={index}
+                  className="col-4 d-flex flex-column align-items-center mb-4"
+                >
+                  <img
+                    className="img-sector"
+                    src={sector.imgurl}
+                    alt="sector img"
+                    style={{ width: '100%', height: '100%' }}
+                  />
+
+                  <p className="sector-text mt-2 mb-4 ">{sector.name}</p>
+                </li>
+              );
+            })}
           </ul>
         </article>
       </section>
